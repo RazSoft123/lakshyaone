b87d401d4d2f289ac73e3d8ee2a3abdf6c03ad16
RazSoft <razgroupgame@gmail.com>
2025-04-21 23:07:12 +0530

    Created login and index view of the component

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,11 @@
+
+<section class="w-full h-dvh relative bg-cover" style="background: url('{{ asset('storage/bg-img.jpg') }}');">
+    <div class="absolute w-full h-full bg-red-300/10"></div>
+    <div class="flex flex-col gap-3 items-center justify-center absolute top-[40%] left-[50%] transform -translate-x-1/2 p-4 w-full">
+        <h1 class="text-2xl font-semibold text-red-50"><span class="text-blue-950">Note</span> what matters</h1>
+        <p class="text-s text-center font-semibold text-red-50">A simple platform for your ideas and memories</p>
+        <div class="w-full flex items-center justify-center">
+            <button class=" text-red-50 py-2 px-4 rounded-[8px] bg-red-500 hover:bg-red-600 text-xs font-medium">Take a note now</button>
+        </div>
+    </div>
+</section>
---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,6 @@
+<nav class="flex justify-center items-center p-4 bg-red-500">
+    <ul class="w-full flex justify-between items-center">
+        <li class="text-xl font-bold text-red-50">LakshyaOne</li>
+        <li class="text-l font-semibold text-blue-950 border-2 p-2 rounded-full hover:text-red-50 hover:border-blue-950 hover:bg-blue-950 transition-all delay-100">Login</li>
+    </ul>
+</nav>
